ba6e822bb93a164864efeeffc44b5da5a0a9b14e
distanceCat <norman.ihle@web.de>
2020-02-04 10:13:58 +0100

    changed venv to amd64

--- goodnews.py
+++ goodnews.py
@@ -1,10 +1,11 @@
 # powered by newsapi.org
 import requests
-import json
-import sentiment_analyser
+# import json
+# import sentiment_analyser
+import sqlite3
+import datetime
 
-
-def fetchNews():
+def fetch_news():
     url = ('https://newsapi.org/v2/top-headlines?'
            'country=de&'
            'apiKey=ad803c9cd6f144a287f50bdbd3e5a502')
@@ -15,11 +16,22 @@ def fetchNews():
         articles.append(article["title"])
         print(article["title"])
 
-    sentiment_analyser.SentimentAnalyser.sample_analyze_sentiment(articles[0])
-
+    # sentiment_analyser.SentimentAnalyser.sample_analyze_sentiment(articles[0])
     # print(news_feed_json["articles"])
     # print(response.json())
+    return articles
+
+def store_titles(articles):
+    conn = sqlite3.connect(".\data\storage.db")
+    c = conn.cursor()
+    dt = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    c.execute("INSERT INTO good_news_short(date, title) VALUES (?, ?)", (dt, articles[0]))
+    conn.commit()
+    conn.close()
+
 
 
 if __name__ == '__main__':
-    fetchNews()
+    fetch_news()
+    articles = fetch_news()
+    store_titles(articles)
--- sentiment_analyser.py
+++ sentiment_analyser.py
@@ -1,24 +1,24 @@
-from google.cloud import language_v1
-from google.cloud.language_v1 import enums
-import six
-
-class SentimentAnalyser():
-
-    def __init__(self):
-        print("init")
-
-    def sample_analyze_sentiment(content):
-        client = language_v1.LanguageServiceClient()
-
-        # content = 'Your text to analyze, e.g. Hello, world!'
-
-        if isinstance(content, six.binary_type):
-            content = content.decode('utf-8')
-
-        type_ = enums.Document.Type.PLAIN_TEXT
-        document = {'type': type_, 'content': content}
-
-        response = client.analyze_sentiment(document)
-        sentiment = response.document_sentiment
-        print('Score: {}'.format(sentiment.score))
-        print('Magnitude: {}'.format(sentiment.magnitude))
+# from google.cloud import language_v1
+# from google.cloud.language_v1 import enums
+# import six
+#
+# class SentimentAnalyser():
+#
+#     def __init__(self):
+#         print("init")
+#
+#     def sample_analyze_sentiment(content):
+#         client = language_v1.LanguageServiceClient()
+#
+#         # content = 'Your text to analyze, e.g. Hello, world!'
+#
+#         if isinstance(content, six.binary_type):
+#             content = content.decode('utf-8')
+#
+#         type_ = enums.Document.Type.PLAIN_TEXT
+#         document = {'type': type_, 'content': content}
+#
+#         response = client.analyze_sentiment(document)
+#         sentiment = response.document_sentiment
+#         print('Score: {}'.format(sentiment.score))
+#         print('Magnitude: {}'.format(sentiment.magnitude))
